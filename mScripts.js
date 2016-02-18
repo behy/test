dbcddba60079408c46b29e4d36127e12ead3fec7
Behy <ble2400bb@gmail.com>
2016-02-18 12:49:35 +0100

    listview

---
+++ mScripts.js
@@ -0,0 +1,9 @@
+			$(document).ready(function() {
+				$("#divTest1").text("Hello");
+				$("#divTest2").text("hoooo");
+				// document.getElementById("divTest2").innerHTML="hi world!";
+				
+				$("div").click(function(){alert("new Alert");});
+				document.getElementById("divTest3").innerHTML="is not o "; 
+				
+			});
